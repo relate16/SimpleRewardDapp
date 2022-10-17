601566f41d23d441f120eb043baa1f2b3b8bff94
전종복 <relate16@naver.com>
2022-10-17 16:01:06 +0900

    reservation / CartItemRepository 같은 중간 엔티티 repository 삭제
    CartItemRepository는 cascade.ALL 설정시 필요하지 않음
    CartItem 같은 중간 엔티티는 주체 엔티티, 즉 Cart에서 cascade.ALL과 같이연결해 놓으면
    Cart로 조회하고 Cart로 저장시키면 되기 때문에
    중간 엔티티Repository, 즉 CartItemRepository는 필요가 없다.

--- src/main/java/com/example/simpleRewardDapp/repository/CartItemRepository.java
+++ src/main/java/com/example/simpleRewardDapp/repository/CartItemRepository.java
@@ -7,6 +7,13 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+/*
+ * 엔티티 설계 다시 할 예정 :
+ * CartItemRepository는 cascade.ALL 설정시 필요하지 않음
+ * CartItem 같은 중간 연결 엔티티는 주체 엔티티, 즉 Cart에서 cascade.ALL과 같이 연결해놓으면
+ * Cart로 조회하고 Cart로 저장시키면 되기 때문에 중간 엔티티 Repository,
+ * 즉 CartItemRepository는필요가 없다.
+ * */
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     @Query("select ci from CartItem ci join ci.item i where i.name = :name")
     List<CartItem> findByItemName(@Param("name") String itemName);
--- src/main/java/com/example/simpleRewardDapp/repository/OrderItemRepository.java
+++ src/main/java/com/example/simpleRewardDapp/repository/OrderItemRepository.java
@@ -3,5 +3,12 @@ package com.example.simpleRewardDapp.repository;
 import com.example.simpleRewardDapp.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+ * 엔티티 설계 다시 할 예정 :
+ * OrderItemRepository는 cascade.ALL 설정시 필요하지 않음
+ * OrderItem 같은 중간 연결 엔티티는 주체 엔티티, 즉 Order에서 cascade.ALL과 같이 연결해놓으면
+ * Order로 조회하고 Order로 저장시키면 되기 때문에 중간 엔티티 Repository,
+ * 즉 OrderItemRepository는필요가 없다.
+ * */
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
